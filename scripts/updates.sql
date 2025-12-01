2e0d17e381cee0c5914a7cffbe52493a81662191
leandropalha <146382155+leandropalha@users.noreply.github.com>
2025-11-30 23:31:12 -0300

    Update email, course name, and semester in database

--- scripts/updates.sql
+++ scripts/updates.sql
@@ -1,1 +1,12 @@
+UPDATE aluno
+SET email = 'novoemail@email.com'
+WHERE id_aluno = 1;
+
+UPDATE curso
+SET nome = 'Sistemas para Internet'
+WHERE id_curso = 10;
+
+UPDATE turma
+SET semestre = 2
+WHERE id_turma = 3;
 
